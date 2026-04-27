ba7c42500a737b42866b508233ef6802113597d9
Miex4857 <Miethede@gmail.com>
2026-04-27 16:01:20 +0200

    prompt

---
+++ Assets/Scripts/PromptData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PromptData", menuName = "Prompt")]
+public class PromptData : ScriptableObject
+{
+    public string PromptHeader;
+    public IndicatorTypes IndicatorType;
+    public bool Expandable;
+}
